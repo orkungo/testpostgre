1df212b125ae161947de22c43ca5560b8fa1a688
orkungo <orkungo@yahoo.com>
2012-09-05 09:49:21 +0200

    'Initial'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testpostgre::Application.config.secret_token = 'bfad4d72d7ed7aefb6e7b88912b2142c25d8d45b46d8a66c53a37a119e8949ee22b058ce8415afa646fa8beeb586b46aedad31290b0c52ad4f37d83b66ed44fa'
